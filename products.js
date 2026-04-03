aec8fd79dfd225c048569b7722785aa08d797e78
grawii <bearniexcutie@gmail.com>
2026-04-03 22:11:26 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,32 @@
+const fontList = [
+    { 
+        name: "เด็กเดค (DekDec)", 
+        family: "DekDec", 
+        price: "129.-",
+        features: ["weight"], // มีแค่ปรับน้ำหนัก
+        weights: ["lighter", "normal", "bold"] 
+    },
+    { 
+        name: "ขีดเขียน (KhitKhian)", 
+        family: "KhitKhian", 
+        price: "149.-",
+        features: ["style"], // มีแค่ปรับโปร่ง/ปกติ
+        styles: ["normal", "outline"] 
+    },
+    { 
+        name: "ไม้ไผ่ (MaiPhai)", 
+        family: "MaiPhai", 
+        price: "159.-",
+        features: ["weight", "style"], // มีทั้งสองอย่าง
+        weights: ["normal", "bold"],
+        styles: ["normal", "outline"]
+    },
+    { 
+        name: "ป๊อกกี้ (GRP Pocky)", 
+        family: "Pocky", 
+        price: "129.-",
+        features: ["weight", "style"], // มีทั้งสองอย่าง
+        weights: ["normal", "bold"],
+        styles: ["normal", "outline"]
+    }
+];
