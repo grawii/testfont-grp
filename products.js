c04fc4dad988f62866eceda49285805cf39f0da2
grawii <bearniexcutie@gmail.com>
2026-04-05 19:40:49 +0700

    Update products.js

--- products.js
+++ products.js
@@ -2,29 +2,27 @@ const fontList = [
     { 
         name: "มนตรา (GRP Mantra)", 
         price: "89.-",
-        mapping: { "บาง": "mantra-thin", "ปกติ": "mantra-normal", "หนา": "mantra-bold" },
-        weights: ["บาง", "ปกติ", "หนา"],
-        styles: ["ปกติ", "โปร่ง"]
+        mapping: { "บาง": "GRPMantrathin", "ปกติ": "GRPMantra", "หนา": "GRPMantrabold" },
+        weights: ["บาง", "ปกติ", "หนา"]
     },
     { 
         name: "ไฮสคูล (GRP HighSchool)", 
         price: "129.-",
-        mapping: { "บาง": "hs-thin", "ปกติ": "hs-normal", "หนา": "hs-bold" },
-        weights: ["บาง", "ปกติ", "หนา"],
-        styles: ["ปกติ", "โปร่ง"] 
+        mapping: { "บาง": "GRPHighSchoolthin", "ปกติ": "GRPHighSchool", "หนา": "GRPHighSchoolbold" },
+        weights: ["บาง", "ปกติ", "หนา"]
     },
     { 
         name: "พัฟฟี่ป็อป (GRP PuffyPop)", 
         price: "69.-",
-        mapping: { "ปกติ": "puffy-normal" },
+        mapping: { "ปกติ": "GRPPuffyPop" },
         weights: ["ปกติ"],
         styles: ["ปกติ", "โปร่ง"]
     },
     { 
         name: "ป๊อกกี้ (GRP Pocky)", 
         price: "89.-",
-        mapping: { "บาง": "pocky-thin", "ปกติ": "pocky-normal", "หนา": "pocky-bold", "3D": "pocky-3d-only" },
+        mapping: { "บาง": "GRP Pocky Thin", "ปกติ": "GRP Pocky", "หนา": "GRP Pocky Bold", "3D": "GRP Pocky 3D" },
         weights: ["บาง", "ปกติ", "หนา"],
-        styles: ["ปกติ", "3D", "โปร่ง"]
+        styles: ["ปกติ", "3D"]
     }
 ];
